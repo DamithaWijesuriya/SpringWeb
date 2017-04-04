01bb24cfc2946b5081ee63ac5dc103cc102667e0
hsenid <damitha@hsenidlanka.com>
2017-04-04 10:16:18 +0530

    Version 1

--- src/main/java/com/springMVCLogin/controller/LoginBean.java
+++ src/main/java/com/springMVCLogin/controller/LoginBean.java
@@ -7,19 +7,23 @@ public class LoginBean {
     private String userName;
     private String password;
 
-    public String getUserName() {
+    public String getUserName()
+    {
         return userName;
     }
 
-    public void setUserName(String userName) {
+    public void setUserName(String userName)
+    {
         this.userName = userName;
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password)
+    {
         this.password = password;
     }
 }
